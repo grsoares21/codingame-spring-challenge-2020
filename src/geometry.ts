548c85a66f6f42e3d9e356c4bd74867262bee8a5
Gabriel Restori Soares <soares-21@hotmail.com>
2020-05-07 20:24:29 +0200

    Go after biggest pebles first and further if bumps enemy

--- src/geometry.ts
+++ src/geometry.ts
@@ -6,3 +6,7 @@ export interface Point {
 export function manhattanDistance(pointA: Point, pointB: Point): number {
   return Math.abs(pointA.x - pointB.x) + Math.abs(pointA.y - pointB.y);
 }
+
+export function areEqual(pointA: Point, pointB: Point): boolean {
+  return pointA.x === pointB.x && pointA.y === pointB.y;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
  **/
 
 import { Point, manhattanDistance } from "./geometry";
+import { areEqual } from "./geometry";
 
 var inputs: string[] = readline().split(" ");
 const width: number = parseInt(inputs[0]); // size of the grid
@@ -11,6 +12,8 @@ for (let i = 0; i < height; i++) {
   const row: string = readline(); // one line of the grid: space " " is floor, pound "#" is wall
 }
 
+let oldPacPoint: Point = { x: -1, y: -1 };
+let bumpIterations = 1;
 // game loop
 while (true) {
   var inputs: string[] = readline().split(" ");
@@ -37,7 +40,9 @@ while (true) {
 
   let minDistance = Number.POSITIVE_INFINITY;
   let currentMinValue = 0;
-  let minDistancePoint: Point;
+  let destinationPoint: Point;
+  let pelletDistanceList: { pelletPoint: Point; pelletDistance: number }[] = [];
+
   for (let i = 0; i < visiblePelletCount; i++) {
     var inputs: string[] = readline().split(" ");
     const x: number = parseInt(inputs[0]);
@@ -46,11 +51,14 @@ while (true) {
     const value: number = parseInt(inputs[2]); // amount of points this pellet is worth
 
     const pelletDistance = manhattanDistance(pacPoint, pelletPoint);
-    if (pelletDistance < minDistance || value > currentMinValue) {
+    if (
+      (pelletDistance < minDistance || value > currentMinValue) &&
+      !areEqual(pacPoint, oldPacPoint)
+    ) {
       currentMinValue = value;
       minDistance = pelletDistance;
-      minDistancePoint = pelletPoint;
-      if (pelletDistance < 4) {
+      destinationPoint = pelletPoint;
+      /*if (pelletDistance < 4) {
         console.error(
           `Min dist: ${minDistance}, Pac point: ${JSON.stringify(
             pacPoint
@@ -58,12 +66,28 @@ while (true) {
             pelletPoint
           )}, pellet value ${value}`
         );
-      }
+      }*/
+    } else if (areEqual(pacPoint, oldPacPoint)) {
+      pelletDistanceList.push({ pelletPoint, pelletDistance });
     }
   }
+  if (!destinationPoint) {
+    pelletDistanceList.sort(
+      (pelletA, pelletB) => pelletA.pelletDistance - pelletB.pelletDistance
+    );
 
+    destinationPoint = pelletDistanceList[bumpIterations].pelletPoint;
+    bumpIterations++;
+  } else {
+    bumpIterations = 1;
+  }
+  console.error(
+    `Bump it: ${bumpIterations}, Dest point: ${JSON.stringify(
+      destinationPoint
+    )}`
+  );
+  console.log(`MOVE 0 ${destinationPoint.x} ${destinationPoint.y}`); // MOVE <pacId> <x> <y>
+  oldPacPoint = pacPoint;
   // Write an action using console.log()
   // To debug: console.error('Debug messages...');
-
-  console.log(`MOVE 0 ${minDistancePoint.x} ${minDistancePoint.y}`); // MOVE <pacId> <x> <y>
 }
