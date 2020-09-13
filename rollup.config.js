3697d18aec59922d5e029c5d713669314a84722d
Gabriel Restori Soares <soares-21@hotmail.com>
2020-09-13 12:49:52 +0200

    Rollup config to include node modules in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 
 import typescript from '@rollup/plugin-typescript'
+import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from "rollup-plugin-node-resolve";
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/main.ts',
@@ -8,7 +11,23 @@ export default {
     format: 'es',
     sourcemap: true
   },
-  plugins: [typescript()],
+  plugins: [typescript(),  commonjs({
+    // non-CommonJS modules will be ignored, but you can also
+    // specifically include/exclude files
+    include: [ "./index.js", "node_modules/**" ], // Default: undefined
+
+    // if true then uses of `global` won't be dealt with by this plugin
+    ignoreGlobal: false, // Default: false
+
+    // if false then skip sourceMap generation for CommonJS modules
+    sourceMap: false // Default: true
+  }),
+
+  nodeResolve({
+    jsnext: true,
+    main: false
+  }),
+  json()],
   watch: {
     include: 'src/**',
     exclude: 'src/**/*.test.ts',
